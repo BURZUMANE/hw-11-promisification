c7ca2b044c0361e494ae51d502f46fdc6e6783c8
Timur Zakirov <1234tima.g@gmail.com>
2020-04-06 18:15:50 +0300

    delta

--- task-03.js
+++ task-03.js
@@ -36,18 +36,18 @@ const randomIntegerFromInterval = (min, max) => {
   /*
    * Должно работать так
    */
-//   makeTransaction({ id: 70, amount: 150 })
-//     .then(logSuccess)
-//     .catch(logError);
+  makeTransaction({ id: 70, amount: 150 })
+    .then(logSuccess)
+    .catch(logError);
   
-//   makeTransaction({ id: 71, amount: 230 })
-//     .then(logSuccess)
-//     .catch(logError);
+  makeTransaction({ id: 71, amount: 230 })
+    .then(logSuccess)
+    .catch(logError);
   
-//   makeTransaction({ id: 72, amount: 75 })
-//     .then(logSuccess)
-//     .catch(logError);
+  makeTransaction({ id: 72, amount: 75 })
+    .then(logSuccess)
+    .catch(logError);
   
-//   makeTransaction({ id: 73, amount: 100 })
-//     .then(logSuccess)
-//     .catch(logError);
+  makeTransaction({ id: 73, amount: 100 })
+    .then(logSuccess)
+    .catch(logError);
